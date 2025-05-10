9f8b50b2ca429a2cd3b60526e5d24fba61a863e6
nerdyfitzy <65741665+nerdyfitzy@users.noreply.github.com>
2025-05-09 22:11:37 -0400

    Create palindromes.go

---
+++ easy/palindromes.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+func isPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	r, _ := regexp.Compile("[^a-z0-9]")
+	s = r.ReplaceAllString(s, "")
+	start, end := 0, len(s)-1
+
+	for start < end {
+		if s[start] != s[end] {
+			return false
+		}
+	}
+	return true
+}
